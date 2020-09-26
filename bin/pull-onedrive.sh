b623b6e8c72460321956ea2cd2d2c4b0899d8549
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-09-26 12:16:32 +0200

    now we can pull/push files/subdirectories if we want to

--- bin/pull-onedrive.sh
+++ bin/pull-onedrive.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $HOME
 
-echo "Copying files from online, possibly overwriting local changes..."
-echo "..waiting for 15 secs"
-sleep 15
-echo "..starting.."
-
-rclone --progress --transfers=64 copy onedrive: OneDrive
+echo "Copying files from online to ~/OneDrive/$1, possibly overwriting local changes..."
+echo "Are you sure? [y/N]"
+read REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Last chance! 15 seconds before it commences!"
+  sleep 15
+  rclone --progress --transfers=64 copy "onedrive:$1" "OneDrive/$1"
+fi
--- bin/push-onedrive.sh
+++ bin/push-onedrive.sh
@@ -2,12 +2,12 @@
 
 cd $HOME
 
-echo "Copying files to online, possibly overwriting online changes..."
+echo "Copying files from ~/OneDrive/$1 to online, possibly overwriting online changes..."
 echo "Are you sure? [y/N]"
 read REPLY
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Last chance! 15 seconds before it commences!"
   sleep 15
-  rclone --progress --transfers=64 copy OneDrive onedrive:
+  rclone --progress --transfers=64 copy "OneDrive/$1" "onedrive:$1"
 fi
