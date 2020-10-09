97177089adac5ecd385211cb31335ee940a07333
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-10-09 13:04:34 +0200

    minor adjustments to my scripts

--- bin/set-brightness.sh
+++ bin/set-brightness.sh
@@ -11,10 +11,13 @@ echo $LEVEL > $HOME/var/last-brightness
 
 LVLA=$(bc <<< "$LEVEL * 0.75")
 LVLB=$(bc <<< "$LEVEL * 0.95")
+LVLC=$(bc <<< "$LEVEL * 1.0")
 
 
 
 set -x
 
 xrandr --output eDP-1-1 --gamma 1:.95:.9 --brightness "$LVLA"
+xrandr --output eDP-1 --gamma 1:.95:.9 --brightness "$LVLA"
 xrandr --output DP-0 --gamma 1:1:1 --brightness "$LVLB"
+xrandr --output DP-2 --gamma 1:1:1 --brightness "$LVLC"
---
+++ bin/set-xrandr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+xrandr --output DP-0 --mode "3840x2160" --pos "0x0"
+xrandr --output eDP-1-1 --mode "3840x2160" --pos "0x2160"
+xrandr --output eDP-1 --mode "3840x2160" --pos "0x2160"
