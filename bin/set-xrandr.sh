aae09aee71fff019202a2e902ca891909587a52f
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-11-15 16:20:38 +0100

    resetting DP-0 panning

--- bin/set-xrandr.sh
+++ bin/set-xrandr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-xrandr --output DP-0 --mode "3840x2160" --pos "0x0"
+xrandr --output DP-0 --mode "3840x2160" --pos "0x0" --panning "3840x2160/3840x2160"
 xrandr --output eDP-1-1 --mode "3840x2160" --pos "0x2160"
 xrandr --output eDP-1 --mode "3840x2160" --pos "0x2160"
 xrandr --output HDMI-0 --mode "1920x1080" --pos "0x2160" --scale 2x2
